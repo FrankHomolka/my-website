3c167c314240ccd96d002cdae6f09c95c81bca6d
Frank Homolka <frank62899@gmail.com>
2021-08-02 12:44:46 -0700

    Cleaned up music page code and removed data to separate file, removed changes to style on hover for mobile and small browsers

---
+++ src/Data/AlbumData.tsx
@@ -0,0 +1,64 @@
+import badDreamsCover from "../Media/albums/bad dreams.jpg";
+import chestnutStreetCover from "../Media/albums/chestnut street.jpg";
+import coffeeRamenCover from "../Media/albums/coffee ramen.jpg";
+import wumpusIICover from "../Media/albums/wumpus II.jpg";
+import desertSongsCover from "../Media/albums/desert songs.jpg";
+import roadHeadCover from "../Media/albums/road head.jpg";
+import songsForCowsCover from "../Media/albums/songs for cows.jpg";
+import stuckOnElevenCover from "../Media/albums/stuck on eleven.jpg";
+import thisAsAMemoryCover from "../Media/albums/this as a memory.jpg";
+import timeMovesSlowCover from "../Media/albums/time moves slow.jpg";
+import wumpusCover from "../Media/albums/wumpus.jpg";
+
+export const albumData = {
+  columnOne: [
+    {
+      img: chestnutStreetCover,
+      link: "https://open.spotify.com/album/2l1ND0USEpYa2mBvu9ypT3?si=6EZTV7amSh6uYtLwhRMoEA&dl_branch=1",
+    },
+    {
+      img: roadHeadCover,
+      link: "https://open.spotify.com/album/0cSmKZBW3hR0XOMLFLaLH6?si=VY9x70TcS7-Yy7lgzVla4A&dl_branch=1",
+    },
+    {
+      img: stuckOnElevenCover,
+      link: "https://open.spotify.com/album/4S5UkN9MFjJlTEU6lpzuXc?si=MoL9K-2XRlq_ueJEL-QNfQ&dl_branch=1",
+    },
+    {
+      img: coffeeRamenCover,
+      link: "https://open.spotify.com/album/4kCbRPCi1pIP3OZV38DjnN?si=I8z0eQQjSOeT7BErbebH0Q&dl_branch=1",
+    },
+  ],
+  columnTwo: [
+    {
+      img: songsForCowsCover,
+      link: "https://open.spotify.com/album/575jS8ZX8Y3SKPHrRjZCDt?si=j63pnfTRT9mEeXHLEkAyNg&dl_branch=1",
+    },
+    {
+      img: wumpusIICover,
+      link: "https://open.spotify.com/album/3h9gtjStj0VZsBTY3HWler?si=y2-JEaprRRClA00r2GbBAA&dl_branch=1",
+    },
+    {
+      img: desertSongsCover,
+      link: "https://open.spotify.com/album/25wXJrFR5oVHpwLsiw2CD1?si=CUdByySwSumxvO-jxVoZqQ&dl_branch=1",
+    },
+  ],
+  columnThree: [
+    {
+      img: timeMovesSlowCover,
+      link: "https://open.spotify.com/album/0m6QgCZhF1UWzuD0afTCJJ?si=ThXBw9DxT9KTuUMHWIHFHg&dl_branch=1",
+    },
+    {
+      img: thisAsAMemoryCover,
+      link: "https://open.spotify.com/album/6wXMBV4yI8frlWDibfHXhy?si=mwL2P4E2R_q__Q7gzPdHMA&dl_branch=1",
+    },
+    {
+      img: wumpusCover,
+      link: "https://open.spotify.com/album/0eNIAZMQROckqBgk6oXf77?si=3DjXotjmTu6F7-x39Na-Jg&dl_branch=1",
+    },
+    {
+      img: badDreamsCover,
+      link: "https://open.spotify.com/album/3uIMZsRdMfbeyMRfRMyxOS?si=7cSBNwf4TlqoiFfTsME6pA&dl_branch=1",
+    },
+  ],
+};
--- src/Pages/MusicPage.tsx
+++ src/Pages/MusicPage.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { useWindowSize } from "../Functions/useWindowSize";
-import badDreamsCover from "../Media/albums/bad dreams.jpg";
-import chestnutStreetCover from "../Media/albums/chestnut street.jpg";
-import coffeeRamenCover from "../Media/albums/coffee ramen.jpg";
-import deltaBluesHotTamalesCover from "../Media/albums/delta blues hot tamales.jpg";
-import wumpusIICover from "../Media/albums/wumpus II.jpg";
-import desertSongsCover from "../Media/albums/desert songs.jpg";
-import roadHeadCover from "../Media/albums/road head.jpg";
-import songsForCowsCover from "../Media/albums/songs for cows.jpg";
-import stuckOnElevenCover from "../Media/albums/stuck on eleven.jpg";
-import thisAsAMemoryCover from "../Media/albums/this as a memory.jpg";
-import timeMovesSlowCover from "../Media/albums/time moves slow.jpg";
-import wumpusCover from "../Media/albums/wumpus.jpg";
 import { theme } from "../ThemeContext";
+import { albumData } from "../Data/AlbumData";
+
+interface IAlbumInfo {
+  img: string;
+  link: string;
+}
 
 export default function MusicPage() {
   const windowSize = useWindowSize();
@@ -23,16 +17,26 @@ export default function MusicPage() {
     width: "22%",
   };
 
-  const imageStyle: React.CSSProperties = {
-    margin: "12px 0",
-    objectFit: windowSize.mobile ? "contain" : "cover",
-    borderRadius: "25px",
-    width: "100%",
-  };
+  function albumTile(albumInfo: IAlbumInfo) {
+    return (
+      <a href={albumInfo.link}>
+        <img
+          className="albumImage"
+          src={albumInfo.img}
+          style={{
+            margin: "12px 0",
+            objectFit: windowSize.mobile ? "contain" : "cover",
+            borderRadius: "25px",
+            width: "100%",
+          }}
+          alt={""}
+        />
+      </a>
+    );
+  }
 
   return (
-    <a
-      href="https://open.spotify.com/artist/3NWBPFKAfqlEIYQ2ltSiNZ?si=jGliKLdLRiKYyi6iaJFs5Q"
+    <div
       style={{
         textDecoration: "none",
         color: "black",
@@ -55,81 +59,23 @@ export default function MusicPage() {
       >
         {/* First column of album covers */}
         <div style={{ ...columnStyle, marginTop: "12%" }}>
-          <img
-            className="albumImage"
-            src={chestnutStreetCover}
-            style={{ ...imageStyle }}
-            alt={""}
-          />
-
-          <img
-            className="albumImage"
-            src={roadHeadCover}
-            style={imageStyle}
-            alt={""}
-          />
-          <img
-            className="albumImage"
-            src={stuckOnElevenCover}
-            style={{ ...imageStyle }}
-            alt={""}
-          />
-          <img
-            className="albumImage"
-            src={coffeeRamenCover}
-            style={{ ...imageStyle }}
-            alt={""}
-          />
+          {albumData.columnOne.map((album: IAlbumInfo) => {
+            return albumTile(album);
+          })}
         </div>
         {/* Second column of album covers */}
         <div style={{ ...columnStyle, width: "44%" }}>
-          <img
-            className="albumImage"
-            src={songsForCowsCover}
-            style={imageStyle}
-            alt={""}
-          />
-          <img
-            className="albumImage"
-            src={wumpusIICover}
-            style={{ ...imageStyle }}
-            alt={""}
-          />
-          <img
-            className="albumImage"
-            src={desertSongsCover}
-            style={{ ...imageStyle }}
-            alt={""}
-          />
+          {albumData.columnTwo.map((album: IAlbumInfo) => {
+            return albumTile(album);
+          })}
         </div>
         {/* Third column of album covers */}
         <div style={{ ...columnStyle, marginTop: "12%" }}>
-          <img
-            className="albumImage"
-            src={timeMovesSlowCover}
-            style={imageStyle}
-            alt={""}
-          />
-          <img
-            className="albumImage"
-            src={thisAsAMemoryCover}
-            style={{ ...imageStyle }}
-            alt={""}
-          />
-          <img
-            className="albumImage"
-            src={wumpusCover}
-            style={imageStyle}
-            alt={""}
-          />
-          <img
-            className="albumImage"
-            src={badDreamsCover}
-            style={imageStyle}
-            alt={""}
-          />
+          {albumData.columnThree.map((album: IAlbumInfo) => {
+            return albumTile(album);
+          })}
         </div>
       </div>
-    </a>
+    </div>
   );
 }
